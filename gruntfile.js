8dd0597eb899ead24605e1c12fd3c0752ecf76b1
1000ch <orz1000ch@gmail.com>
2013-02-14 19:24:22 +0900

    update package.json

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,26 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
+		jshint: {
+			all: ["./handle.js"]
+		},
+		uglify: {
+			my_target: {
+				files: {
+					"./handle.min.js": ["./handle.js"]
+				}
+			}
+		},
 		qunit: {
 			all: ["test/*.html"]
 		},
 		watch: {
 			files: ["handle.js"],
-			tasks: ["qunit"]
+			tasks: ["jshint", "uglify", "qunit"]
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
